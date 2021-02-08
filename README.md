690abc111718adc6ce305f6b4ca201dc4cde6f4b
Josh Ziegler <josh@pylon.com>
2021-02-08 14:22:38 -0500

    update dependencies; readme updates

--- README.md
+++ README.md
@@ -5,17 +5,13 @@ This is a simple Android app that demonstrates the core features of [Spokestack 
 ---
 ## Prerequisites/Notes
 
-### Android NDK setup
-
-Because Spokestack relies on native libraries for some audio processing tasks to optimize performance, you'll need the Android NDK to build this project. You can install it by opening the SDK manager in Android Studio, selecting the SDK Tools tab, and setting the `ndkVersion` property in the `android` block of  your app-level `build.gradle` file. The NDK can also be downloaded [directly from Google](https://developer.android.com/ndk/downloads). The property has been set in this project to the current version at the time of writing, so it may need to be updated.
-
 
 ### Wakeword detection
 
 Just like the skeleton project mentioned above, this demo is configured to use wakeword detection provided by [TensorFlow Lite](https://www.tensorflow.org/lite) models. These models are trained to respond to the "Spokestack" wakeword. They must be downloaded and placed in `src/main/assets`. The file names should not be changed.
-- [detect](https://d3dmqd7cy685il.cloudfront.net/model/wake/spokestack/detect.lite)
-- [encode](https://d3dmqd7cy685il.cloudfront.net/model/wake/spokestack/encode.lite)
-- [filter](https://d3dmqd7cy685il.cloudfront.net/model/wake/spokestack/filter.lite)
+- [detect](https://d3dmqd7cy685il.cloudfront.net/model/wake/spokestack/detect.tflite)
+- [encode](https://d3dmqd7cy685il.cloudfront.net/model/wake/spokestack/encode.tflite)
+- [filter](https://d3dmqd7cy685il.cloudfront.net/model/wake/spokestack/filter.tflite)
 
 ### Speech recognition
 
@@ -29,13 +25,13 @@ If you're unable to get on-device ASR to work, you can try using a different ASR
 
 Here's a screenshot of `MainActivity` in action, so we can talk about what's going on.
 
-![](images/screenshot.png "screenshot")
+<img src="images/screenshot.png" alt="Spokestack Control Room screenshot" width="400"/>
 
-A Spokestack `SpeechPipeline` is built and started after the audio recording permission is granted, so the microphone is active, and Spokestack is waiting to hear its wakeword. The text next to the `Voice` label will display `Active` whenever _any_ speech is detected (otherwise known as [voice activity detection](https://en.wikipedia.org/wiki/Voice_activity_detection)). Random room noise will occasionally be detected as speech.
+A `Spokestack` instance is built and started after the audio recording permission is granted, so the microphone is active, and Spokestack is waiting to hear its wakeword. The text next to the `Voice` label will display `Active` whenever _any_ speech is detected (otherwise known as [voice activity detection](https://en.wikipedia.org/wiki/Voice_activity_detection)). Random room noise will occasionally be detected as speech.
 
-If the word "spokestack" is detected by the on-device wakeword model (or if the `Activate ASR` button is tapped), the text next to the `ASR` label will display `Active`, indicating that speech is being sent to the chosen speech recognition provider. Errors during this process are displayed as toasts at the bottom of the screen. When ASR is done detecting speech (after the user stops speaking for a configurable amount of time; again, see the configuration documentation for more info), the text of the detected speech will be displayed right under the `Speech Transcript:` label.
+If the word "spokestack" is detected by the on-device wakeword model (or if the `Activate ASR` button is tapped), the text next to the `ASR` label will display `Active`, indicating that speech is being sent to the chosen speech recognition provider. Errors during this process are displayed as toasts at the bottom of the screen. When ASR is done detecting speech (after the user stops speaking for a configurable amount of time; again, see the configuration documentation for more info), the text of the detected speech will be displayed under the `Transcript` label.
 
-The bottom part of the screen is for testing Spokestack's TTS service. If you'd like Spokestack to simply echo back what you've said, toggle the `Update from transcript` switch; otherwise you can enter the text to be spoken into the text field just above the `Speak` button.
+The bottom part of the screen is for testing Spokestack's NLU and TTS services. If you'd like Spokestack to use the ASR transcript, toggle the `Update from transcript` switch; otherwise you can enter the text to be classified or spoken into the text field just above the `Speak` button.
 
 That's it! Happy testing!
 
@@ -43,10 +39,15 @@ That's it! Happy testing!
 
 ## Device compatibility
 
-This chart lists physical devices on which the Control Room app has been tested by either the Spokestack team or our community. If you have a device that is not listed, please try it out and [submit a PR](https://github.com/spokestack/android-control-room/pulls) with your results!
+This chart lists physical devices on which Android ASR has been tested by either the Spokestack team or our community. If you have a device that is not listed, please try it out and [submit a PR](https://github.com/spokestack/android-control-room/pulls) with your results!
 
-| Device           | API Level | Launches?          | On-device ASR?     |
-| -------------    | --------- | ------------------ | ------------------ |
-| Moto X (2nd Gen) |        22 | :white_check_mark: | :x:                |
-| Pixel 1          |        29 | :white_check_mark: | :x:                |
-| Pixel 3 XL       |        29 | :white_check_mark: | :white_check_mark: |
+| Device                 | API Level | ASR working?  |
+| ---------------------- | --------- | ------------  |
+| Moto X (2nd Gen)       |        22 | &#10060;  `*` |
+| Lenovo TB-X340F tablet |        27 | &#9989;       |
+| Pixel 1                |        29 | &#9989;       |
+| Pixel 3 XL             |        29 | &#9989;       |
+| Pixel 3a               |        29 | &#9989;       |
+| Pixel 4                |        29 | &#9989;       |
+
+`*` ASR fails consistently with a `SERVER_ERROR`, which seems to indicate that the server used by the device manufacturer to handle these requests is no longer operational.
--- app/src/main/java/io/spokestack/controlroom/MainActivity.kt
+++ app/src/main/java/io/spokestack/controlroom/MainActivity.kt
@@ -11,22 +11,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import io.spokestack.spokestack.OnSpeechEventListener
-import io.spokestack.spokestack.SpeechContext
-import io.spokestack.spokestack.SpeechPipeline
+import io.spokestack.spokestack.*
 import io.spokestack.spokestack.nlu.NLUResult
-import io.spokestack.spokestack.nlu.TraceListener
-import io.spokestack.spokestack.nlu.tensorflow.TensorflowNLU
 import io.spokestack.spokestack.tts.SynthesisRequest
 import io.spokestack.spokestack.tts.TTSEvent
-import io.spokestack.spokestack.tts.TTSListener
-import io.spokestack.spokestack.tts.TTSManager
 import io.spokestack.spokestack.util.EventTracer
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
 
@@ -34,9 +24,13 @@ private const val PREF_NAME = "AppPrefs"
 private const val versionKey = "versionCode"
 private const val nonexistent = -1
 
-class MainActivity : AppCompatActivity(), OnSpeechEventListener, TTSListener, TraceListener {
+class MainActivity : AppCompatActivity() {
     private val logTag = javaClass.simpleName
+
+    // a sentinel value we'll use to verify we have the proper permissions to use Spokestack to
+    // record audio
     private val audioPermission = 1337
+
     private val redColor: Int by lazy(LazyThreadSafetyMode.NONE) {
         ContextCompat.getColor(applicationContext, R.color.red)
     }
@@ -44,23 +38,22 @@ class MainActivity : AppCompatActivity(), OnSpeechEventListener, TTSListener, Tr
         ContextCompat.getColor(applicationContext, R.color.green)
     }
 
-    private var pipeline: SpeechPipeline? = null
-    private var nlu: TensorflowNLU? = null
-    private var tts: TTSManager? = null
+    // the Spokestack instance itself and a listener to receive events from its modules
+    private lateinit var spokestack: Spokestack
+    private val listener: SpokestackAdapter = SpokestackListener()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        echoTranscript.setOnCheckedChangeListener(::echoChanged)
-
         // Spokestack setup
-
-        if (this.pipeline == null && checkMicPermission()) {
-            buildPipeline()
+        spokestack = buildSpokestack()
+        checkMicPermission()
+        if (checkMicPermission()) {
+            spokestack.start()
         }
-        buildNLU()
-        buildTTS()
+
+        echoTranscript.setOnCheckedChangeListener(::echoChanged)
     }
 
     @Suppress("UNUSED_PARAMETER")
@@ -70,56 +63,63 @@ class MainActivity : AppCompatActivity(), OnSpeechEventListener, TTSListener, Tr
         }
     }
 
-    private fun checkMicPermission(): Boolean {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-            == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        }
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.RECORD_AUDIO),
-            audioPermission
-        )
-        return false
+    @Suppress("UNUSED_PARAMETER")
+    fun activateAsrTapped(view: View) {
+        spokestack.activate()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            audioPermission -> {
-                if (grantResults.isNotEmpty() &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED
-                ) {
-                    buildPipeline()
-                } else {
-                    Log.w(logTag, "Record permission not granted; voice control disabled!")
-                }
-                return
-            }
-            else -> {
-                // do nothing
-            }
-        }
+    @Suppress("UNUSED_PARAMETER")
+    fun speakTapped(view: View) {
+        val request = SynthesisRequest.Builder(ttsInput.text)
+            .withMode(SynthesisRequest.Mode.MARKDOWN)
+            .build()
+        spokestack.synthesize(request)
+        setTTSProcessing(true)
     }
 
-    private fun buildPipeline() {
+    @Suppress("UNUSED_PARAMETER")
+    fun classifyTapped(view: View) {
+        spokestack.classify(ttsInput.text.toString())
+    }
+
+    private fun buildSpokestack(): Spokestack {
+        // We're going to use TensorFlow Lite models for wakeword detection to demonstrate how
+        // you'd set up the speech pipeline to expect them. Compressing them in your assets folder
+        // and decompressing them to the cache folder if they're absent is one way to keep app size
+        // down while making the models available at runtime. Another way would be to download the
+        // models from a CDN and cache them in a similar fashion.
+        // You don't need to worry about this step if you're not using TFLite for wakeword
+        // detection (see the io.spokestack.spokestack.profile package for descriptions of
+        // the various pre-configured profiles available).
+        // See the README for links to the original files.
         checkForModels()
 
-        pipeline = SpeechPipeline.Builder()
-            .useProfile("io.spokestack.spokestack.profile.TFWakewordAndroidASR")
-            .setProperty("wake-detect-path", "$cacheDir/detect.lite")
-            .setProperty("wake-encode-path", "$cacheDir/encode.lite")
-            .setProperty("wake-filter-path", "$cacheDir/filter.lite")
-            .setAndroidContext(applicationContext)
-            .addOnSpeechEventListener(this)
+        // On-device ASR via the "TFWakewordAndroidASR" profile is enabled by the Spokestack
+        // builder by default. Note that it may be unavailable on older devices, so you may wish
+        // to call `SpeechRecognizer.isRecognitionAvailable(Context)` and use a different ASR if
+        // it is unavailable, or use the Android-provided ASR only for demos.
+        return Spokestack.Builder()
+            // wakeword models
+            .setProperty("wake-detect-path", "$cacheDir/detect.tflite")
+            .setProperty("wake-encode-path", "$cacheDir/encode.tflite")
+            .setProperty("wake-filter-path", "$cacheDir/filter.tflite")
+            // NLU
+            // you'll need NLU models to use this component; see the README for more
+            // information on obtaining them
+            .setProperty("nlu-model-path", "$cacheDir/note.tflite")
+            .setProperty("nlu-metadata-path", "$cacheDir/note.json")
+            .setProperty("wordpiece-vocab-path", "$cacheDir/vocab.txt")
             .setProperty("trace-level", EventTracer.Level.DEBUG.value())
+            // TTS
+            .setProperty("spokestack-id", "f0bc990c-e9db-4a0c-a2b1-6a6395a3d97e")
+            .setProperty(
+                "spokestack-secret",
+                "5BD5483F573D691A15CFA493C1782F451D4BD666E39A9E7B2EBE287E6A72C6B6"
+            )
+            .withAndroidContext(applicationContext)
+            // make sure we receive Spokestack events
+            .addListener(listener)
             .build()
-
-        pipeline?.start()
     }
 
     private fun checkForModels() {
@@ -129,7 +129,6 @@ class MainActivity : AppCompatActivity(), OnSpeechEventListener, TTSListener, Tr
             val currentVersionCode = BuildConfig.VERSION_CODE
             val prefs = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
             val savedVersionCode = prefs.getInt(versionKey, nonexistent)
-
             if (currentVersionCode != savedVersionCode) {
                 decompressModels()
 
@@ -140,105 +139,72 @@ class MainActivity : AppCompatActivity(), OnSpeechEventListener, TTSListener, Tr
     }
 
     private fun modelsCached(): Boolean {
-        val filterName = "filter.lite"
+        // We'll use the presence of one of the wakeword models as a proxy for everything being
+        // decompressed since we do it all in the same step. You may wish to be more thorough.
+        val filterName = "filter.tflite"
         val filterFile = File("$cacheDir/$filterName")
         return filterFile.exists()
     }
 
     private fun decompressModels() {
         listOf(
-            "detect.lite", "encode.lite", "filter.lite", "nlu.lite", "nlu.json",
+            "detect.tflite",
+            "encode.tflite",
+            "filter.tflite",
+            "note.tflite",
+            "note.json",
             "vocab.txt"
-        )
-            .forEach(::cacheAsset)
+        ).forEach(::cacheAsset)
     }
 
-    private fun cacheAsset(modelName: String) {
-        val filterFile = File("$cacheDir/$modelName")
-        val inputStream = assets.open(modelName)
+    private fun cacheAsset(fileName: String) {
+        val cachedFile = File("$cacheDir/$fileName")
+        val inputStream = assets.open(fileName)
         val size = inputStream.available()
         val buffer = ByteArray(size)
         inputStream.read(buffer)
         inputStream.close()
-        val fos = FileOutputStream(filterFile)
+        val fos = FileOutputStream(cachedFile)
         fos.write(buffer)
         fos.close()
     }
 
-    private fun buildNLU() {
-        if (this.nlu == null) {
-            this.nlu = TensorflowNLU.Builder()
-                .setProperty("nlu-model-path", "$cacheDir/nlu.lite")
-                .setProperty("nlu-metadata-path", "$cacheDir/nlu.json")
-                .setProperty("wordpiece-vocab-path", "$cacheDir/vocab.txt")
-                .setProperty("trace-level", EventTracer.Level.DEBUG.value())
-                .addTraceListener(this)
-                .build()
+    private fun checkMicPermission(): Boolean {
+        // On API levels >= 23, users can revoke permissions at any time, and API levels >= 26
+        // require the RECORD_AUDIO permission to be requested at runtime, so we'll need
+        // to verify it on launch
+        val recordPerm = Manifest.permission.RECORD_AUDIO
+        val granted = PackageManager.PERMISSION_GRANTED
+        if (ContextCompat.checkSelfPermission(this, recordPerm) == granted) {
+            return true
         }
+        ActivityCompat.requestPermissions(this, arrayOf(recordPerm), audioPermission)
+        return false
     }
 
-    private fun buildTTS() {
-        if (this.tts == null) {
-            this.tts = TTSManager.Builder()
-                .setTTSServiceClass("io.spokestack.spokestack.tts.SpokestackTTSService")
-                .setOutputClass("io.spokestack.spokestack.tts.SpokestackTTSOutput")
-                .setProperty("spokestack-id", "f0bc990c-e9db-4a0c-a2b1-6a6395a3d97e")
-                .setProperty(
-                    "spokestack-secret",
-                    "5BD5483F573D691A15CFA493C1782F451D4BD666E39A9E7B2EBE287E6A72C6B6"
-                )
-                .addTTSListener(this)
-                .setAndroidContext(applicationContext)
-                .setLifecycle(lifecycle)
-                .build()
-        }
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    fun activateAsrTapped(view: View) {
-        pipeline?.activate()
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    fun speakTapped(view: View) {
-        val request = SynthesisRequest.Builder(ttsInput.text)
-            .withMode(SynthesisRequest.Mode.MARKDOWN)
-            .build()
-        tts?.synthesize(request)
-        setTTSProcessing(true)
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    fun classifyTapped(view: View) {
-        classify(ttsInput.text.toString())
-    }
-
-    private fun classify(utterance: String) {
-        GlobalScope.launch(Dispatchers.Default) {
-            nlu?.let {
-                val result = it.classify(utterance).get()
-                withContext(Dispatchers.Main) {
-                    setNluResults(result)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        val granted = PackageManager.PERMISSION_GRANTED
+        // respond to the permission request's asynchronous result
+        when (requestCode) {
+            audioPermission -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == granted) {
+                    // if you request permissions when, e.g., a microphone button is tapped, you
+                    // may wish to call `activate()` here instead of `start()`
+                    spokestack.start()
+                } else {
+                    Log.w(logTag, "Record permission not granted; voice control disabled!")
                 }
+                return
+            }
+            else -> {
+                // do nothing
             }
         }
     }
-
-// async-er method
-//    private fun classify(utterance: String) {
-//        val result = nlu?.classify(utterance)
-//        result?.registerCallback(object : Callback<NLUResult> {
-//            override fun call(nluResult: NLUResult?) {
-//                runOnUiThread {
-//                    setNluResults(nluResult!!)
-//                }
-//            }
-//
-//            override fun onError(err: Throwable?) {
-//                errorToast(err?.localizedMessage!!)
-//            }
-//        })
-//    }
 
     private fun setNluResults(result: NLUResult) {
         result.error?.printStackTrace()
@@ -294,33 +260,7 @@ class MainActivity : AppCompatActivity(), OnSpeechEventListener, TTSListener, Tr
             "false" -> setVadActive(false)
             // not a VAD trace; do nothing
             else -> if (!message.startsWith("agc", false)) {
-                    println(message)
-                }
-        }
-    }
-
-    // OnSpeechEventListener implementation
-    override fun onEvent(event: SpeechContext.Event?, context: SpeechContext?) {
-        when (event) {
-            SpeechContext.Event.ACTIVATE -> setAsrActive(true)
-            SpeechContext.Event.DEACTIVATE -> setAsrActive(false)
-            SpeechContext.Event.RECOGNIZE -> context?.transcript?.let { transcribe(it) }
-            SpeechContext.Event.TIMEOUT -> errorToast("ASR timeout")
-            SpeechContext.Event.ERROR -> context?.error?.message?.let {
-                context.error.printStackTrace()
-                errorToast("${context.error.javaClass}: $it")
-            }
-            SpeechContext.Event.TRACE -> context?.message?.let { handleTrace(it) }
-        }
-    }
-
-    // TTSListener implementation
-    override fun eventReceived(event: TTSEvent?) {
-        when (event?.type) {
-            TTSEvent.Type.ERROR -> event.error.message?.let { errorToast("TTS error: $it") }
-            TTSEvent.Type.AUDIO_AVAILABLE -> {
-                println(event.ttsResponse.audioUri)
-                setTTSProcessing(false)
+                println(message)
             }
         }
     }
@@ -344,7 +284,65 @@ class MainActivity : AppCompatActivity(), OnSpeechEventListener, TTSListener, Tr
         }
     }
 
-    override fun onTrace(level: EventTracer.Level?, message: String?) {
-        println("$level: $message")
+    inner class SpokestackListener : SpokestackAdapter() {
+
+        override fun onEvent(event: SpeechContext.Event, context: SpeechContext) {
+            when (event) {
+                SpeechContext.Event.ACTIVATE -> setAsrActive(true)
+                SpeechContext.Event.DEACTIVATE -> setAsrActive(false)
+                SpeechContext.Event.PARTIAL_RECOGNIZE, SpeechContext.Event.RECOGNIZE ->
+                    context.transcript?.let { transcribe(it) }
+                SpeechContext.Event.TIMEOUT -> errorToast("ASR timeout")
+                SpeechContext.Event.ERROR -> context.error?.message?.let {
+                    context.error.printStackTrace()
+                    errorToast("${context.error.javaClass}: $it")
+                }
+                SpeechContext.Event.TRACE -> context.message?.let { handleTrace(it) }
+            }
+        }
+
+        // NLU
+        override fun call(result: NLUResult) {
+            setNluResults(result)
+            respond(result.utterance)
+        }
+
+        private fun respond(utterance: String) {
+            // A (too-) simple response generator that parrots back what the user just said. With
+            // the default TTS setup, this response will be automatically played when the audio is
+            // available.
+            val request = SynthesisRequest.Builder("Why do you feel that $utterance?").build()
+            spokestack.synthesize(request)
+        }
+
+        // TTS listener implementation
+        // We're letting Spokestack automatically handle TTS playback,
+        // so this isn't strictly necessary,
+        // but it's here to provide error feedback
+        override fun ttsEvent(event: TTSEvent) {
+            when (event.type) {
+                TTSEvent.Type.ERROR -> Log.w(logTag, event.error)
+                // If you're managing playback yourself, this is where you'd receive the URL to your
+                // synthesized audio
+                TTSEvent.Type.AUDIO_AVAILABLE -> {
+                    Log.i(logTag, "Audio received: ${event.ttsResponse.audioUri}")
+                    setTTSProcessing(false)
+                }
+                TTSEvent.Type.PLAYBACK_COMPLETE -> Log.i(logTag, "TTS playback complete")
+                else -> {
+                    // do nothing
+                }
+            }
+        }
+
+        override fun onTrace(level: EventTracer.Level, message: String) {
+            when (level) {
+                EventTracer.Level.ERROR -> Log.e(logTag, message)
+                EventTracer.Level.DEBUG -> Log.d(logTag, message)
+                EventTracer.Level.INFO -> Log.i(logTag, message)
+                EventTracer.Level.WARN -> Log.w(logTag, message)
+                else -> Log.v(logTag, message)
+            }
+        }
     }
 }
